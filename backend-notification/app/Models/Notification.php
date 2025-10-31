199ddf754f66ad0e99715e85630583001a2d4795
amadoxxo <elian12478amadiss@gmail.com>
2025-10-31 14:01:38 -0500

    Migracion de la tabla users y notification

---
+++ backend-notification/app/Models/Notification.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    public $timestamps = false;
+
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'message',
+        'read',
+    ];
+
+    protected $casts = [
+        'read' => 'boolean'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+}
--- backend-notification/database/migrations/2025_10_31_182033_create_notifications_table.php
+++ backend-notification/database/migrations/2025_10_31_182033_create_notifications_table.php
@@ -11,12 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->timestamps();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->string('message');
+            $table->boolean('read')->default(false);
         });
     }
 
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('notifications');
     }
 };
